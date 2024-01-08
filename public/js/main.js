25a510d6f3518b9893b79e761827a8db33c6e4a2
57568 <57568@etu.he2b.be>
2024-01-08 11:24:59 +0100

    class Hero creation

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,11 @@ class Boss {
     }
 }
 
+class Heros {
+    constructor(name,combatPosition,attack,hp){
+        this.name = name
+        this.combatPosition = combatPosition
+        this.attack = attack
+        this.hp = hp
+    }
+}
