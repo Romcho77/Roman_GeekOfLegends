268e4694a6b3524e667fe5b1b50a3b87826f26e0
57568 <57568@etu.he2b.be>
2024-01-08 11:41:11 +0100

    creation of a class Warrior

--- public/js/main.js
+++ public/js/main.js
@@ -16,6 +16,25 @@ class Hero {
     combatStatement(){
         if(this.combatPosition == "attack"){
             this.attack = this.attack + this.attack*0.2
+            return 1
+        }else if(this.combatPosition == "defense"){
+            return 0.5
+        }else{
+            console.log("Je ne comprends pas dans quel posutre de combat ton heros est?");
+            return 0
         }
     }
+}
+
+class Guerrier extends Hero{
+    constructor(name,combatPosition,attack,hp,ragePoint){
+        this.name = name
+        this.combatPosition = combatPosition
+        this.attack = attack
+        this.hp = hp
+        this.ragePoint = ragePoint
+
+    }
+
+    
 }
